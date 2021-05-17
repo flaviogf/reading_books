194c33d6a5ae40b02c5d35609078e27db9102310
flaviogf <flavio.fernandes6@gmail.com>
2021-05-17 11:13:55 -0300

    feat: adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create title: 'Clean Code', author: 'Robert C. Martin', cover: 'https://via.placeholder.com/133x191', status: :to_read
+Book.create title: 'Refactoring', author: 'Martin Fowler', cover: 'https://via.placeholder.com/133x191', status: :reading
+Book.create title: 'Extreme Programming', author: 'Vinicius Teles', cover: 'https://via.placeholder.com/133x191', status: :read
